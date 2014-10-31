c24494ea61f1d6a8617c69b5daca8c3c3f9656aa
Michael Woo <woomike@gmail.com>
2014-10-31 16:11:32 -0700

    refactored app.js and added restart button

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
 
 $(document).ready(function() {
-	var aString = "Is this what you want, Brian?!\
-	\nCause this is what I built\
-	";
+	var aString = "Is this what you want, Brian?!\nCause this is what I built\n";
+	var subStringIndex = 0;
 	
 	//Print partial string to 'p' tag in html. index, end position of substring
-	function printSubString(text, index){
+	function printStringIndex(text, index){
 		var nString = text.substring(0,index);
 		$('p').text(nString + "_");
 	}
 
-	
-	//Set i as indicator of string position
-	var i = 0;
-	//Start animation on document ready, refresh 70/1000 sec
-	animate = setInterval(
-		function(){
-			//run until whole string has been printed
-			if(i < aString.length){
-				printSubString(aString, i);
-				i++;
-			}
-		} , 70);
+	function checkSubStringPosition() {
+		//run until whole string has been printed
+		if(subStringIndex < aString.length) {
+			printStringIndex(aString, subStringIndex);
+			subStringIndex++;
+		}
+	}
 
-	
+	function printSubString() {
+		//Start animation on document ready, refresh 70/1000 sec
+		setInterval(checkSubStringPosition, 70);
+	}
+
+	function resetSubString() {
+		$('p').empty();
+		subStringIndex = 0;
+		printSubString();
+	}
+
+	printSubString();
+
+	$('button').click(resetSubString);
 });
 
